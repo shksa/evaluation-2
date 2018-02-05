582f6e8235b82258e586fd632248405e3d7c629d
sreekar <sreekar339@gmail.com>
2018-02-05 11:10:35 +0530

    api/prob1 - newfile added for testing second api - test working

--- src/API1/getRatings.js
+++ src/API1/getRatings.js
@@ -3,14 +3,15 @@ const request = require('request');
 
 const server = new Hapi.Server();
 server.connection({ port: 8004, host: 'localhost' });
-const api1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
+// const api1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
 const api2 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/';
 
 server.route({
-  path: '/getBooks',
+  path: '/{id}',
   method: 'GET',
   handler: (req, reply) => {
     // let responseJSON;
+    const url = api2 + req.params.id;
     request.get(url, (error, response, body) => {
       const json = JSON.parse(body);
       // console.log(json);
